43546dbb4f1c63be2dde52eb7d4c3d1a7170f4fd
lekenneth77 <lekenneth77@gmail.com>
2023-06-08 11:25:57 -0500

    enabledisable controls and stun stars

---
+++ Assets/Scripts/BoardGame/Minigames/TreeHop/StunStarSpin.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StunStarSpin : MonoBehaviour
+{
+    public float speed = 1.5f;
+    public float activeTime = 3f;
+
+    public bool trigger = false;
+    
+    public void StartSpin() {
+        gameObject.SetActive(true);
+        trigger = true;
+        StartCoroutine("EndSpin");
+    }
+
+    private IEnumerator EndSpin() {
+        yield return new WaitForSeconds(activeTime);
+        trigger = false;
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!trigger) { return; }
+        gameObject.transform.Rotate(0, speed, 0);
+    }
+}
--- Assets/Scripts/BoardGame/Minigames/TreeHop/TreeHop.cs
+++ Assets/Scripts/BoardGame/Minigames/TreeHop/TreeHop.cs
@@ -21,14 +21,44 @@ public class TreeHop : Minigame, Controls.ITreeHopActions
     public void OnA(InputAction.CallbackContext context)
     {
         if (!context.performed) { return; }
-        playerOne.Jump(true);
+        int result = playerOne.Jump(true);
+        AfterJump(playerOne, result, "A", "D");
     }
 
     public void OnD(InputAction.CallbackContext context)
     {
         if (!context.performed) { return; }
-        playerOne.Jump(false);
-
+        int result = playerOne.Jump(false);
+        AfterJump(playerOne, result, "A", "D");
     }
+
+    private void AfterJump(TreeTrunk player, int result, string inputOne, string inputTwo)
+    {
+        if (result == -1) {
+            //failed jump
+            StartCoroutine(DisableEnable(player, 3f));
+        } else if (result == 0) {
+            //success jump
+            StartCoroutine(DisableEnable(player, 0.5f));
+        } else { 
+            //reached finish
+        }
+    }
+
+    private IEnumerator DisableEnable(TreeTrunk player, float disableTime)
+    {
+        if (player == playerOne) {
+            controls.TreeHop.A.Disable();
+            controls.TreeHop.D.Disable();
+        }
+        yield return new WaitForSeconds(disableTime);
+        if (player == playerOne)
+        {
+            controls.TreeHop.A.Enable();
+            controls.TreeHop.D.Enable();
+        }
+        Debug.Log("Reenabled!");
+    }
+
 
 }
